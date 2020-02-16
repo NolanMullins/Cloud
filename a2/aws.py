8ec253ce85a66ca08dfd8d5221c346fe5bfa3f22
NolanMullins <nolan.mullins@me.com>
2020-02-16 11:26:46 -0500

    Working on creating instance

--- a2/aws.py
+++ a2/aws.py
@@ -9,6 +9,7 @@ import json
 import csv
 import decimal
 import boto3
+import os
 
 from botocore.exceptions import ClientError
 
@@ -32,16 +33,29 @@ def readFiles():
     return vms, docker
 
 
+def createAWSVM(ec2, vm, docker):
+    print("Creating / Starting: "+vm[2])
+    #Create ssh key
+    outfile = open('keys/'+vm[7],'w')
+    key_pair = ec2.create_key_pair(KeyName='myKey')
+    keyPairOut = str(key_pair.key_material)
+    outfile.write(keyPairOut)
+
+    #Create instance 
+    ec2.create_instances(ImageId=vm[1], MinCount=1, MaxCount=1, InstanceType=vm[3], KeyName='myKey')
+
 if __name__ == "__main__":
 
     print("""
 ************
 Welcome
 ************""")
-    ec2 = boto3.client('ec2')
-    ec2_resource = boto3.resource('ec2')
+    ec2 = boto3.client('ec2', region_name='us-east-1')
+    ec2_resource = boto3.resource('ec2', region_name='us-east-1')
 
     vms, docker = readFiles()
+    for vm in vms:
+        if vm[0]=='AWS':
+            createAWSVM(ec2_resource, vm, docker)
 
-    print(vms)
-    print(docker)
+
